ed1fc5c83f1bc6aaccd5c3d527cd21879c3144f9
Dineshkuna <kunadinesh.work@gmail.com>
2026-01-06 19:11:29 +0530

    single blog inprogress

--- client/src/components/SingleBlog.jsx
+++ client/src/components/SingleBlog.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link, useParams} from 'react-router-dom';
+import { BASEURL } from '../utils/util.config';
+
 
 const SingleBlog = () => {
+  const [blog, setBlog] = useState([]);
+  const {id} = useParams();
+  console.log(id);
+
+
+  useEffect(() => {
+    getBlogData();
+  },[]);
+
+
+  const getBlogData = async () => {
+    let res =  await fetch(`${BASEURL}/blog/getblogbyid/${id}`);
+    let data = await res.json();
+
+    setBlog(data);
+  }
+  
   return (
-    <div>
-        Single Blog Page
-      
-    </div>
+
+    // console.log(blog)
+    <>
+        <div className="mt-3 mb-5">
+          <img src={`https://www.theblogstarter.com/wp-content/uploads/2014/02/4.jpg`} alt="" className='img-fluid'/>
+
+          <div className="mt-3">
+            <h5 className="card-title">{blog.title}</h5>
+            <p className="card-text">{blog.topic}</p>
+            <p className="card-text">{blog.content}</p>
+          </div>
+        </div>
+
+    </>
   )
 }
 
